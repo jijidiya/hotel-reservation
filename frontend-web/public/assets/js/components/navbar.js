4f872396d2ae5049c1cf48674ede6f1bf84523dc
Mr-magni <magnificatkakanda@gmail.com>
2025-12-29 03:38:27 +0100

    Update navbar.js

---
+++ frontend-web/public/assets/js/components/navbar.js
@@ -0,0 +1,14 @@
+// navbar.js
+document.getElementById('navbar').innerHTML = `
+<nav>
+  <h1>HotelBooking</h1>
+  <ul>
+    <li><a href="index.html">Accueil</a></li>
+    <li><a href="chambres.html">Chambres</a></li>
+    <li><a href="reservation.html">Réserver</a></li>
+    <li><a href="connexion.html">Connexion</a></li>
+    <li><a href="enregistrer.html">S’inscrire</a></li>
+    <li><a href="profil.html">Profil</a></li>
+  </ul>
+</nav>
+`;
